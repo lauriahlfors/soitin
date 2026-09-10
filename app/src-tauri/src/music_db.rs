22e0e0fd9287a11567fb791285f727e07b1660ce
lauriahlfors <lauriahlfors@proton.me>
2026-09-10 09:27:53 +0300

    feat(backend): add function to initialize sqlite database tables

---
+++ app/src-tauri/src/music_db.rs
@@ -0,0 +1,49 @@
+use sqlx::sqlite::SqlitePool;
+
+pub async fn initialize_db(pool: &SqlitePool) -> sqlx::Result<(), String> {
+    let mut transaction_pool = pool.begin().await.map_err(|e| e.to_string())?;
+
+    // Album table
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS albums (
+            id              TEXT PRIMARY KEY,
+            title           TEXT NOT NULL,
+            artist          TEXT NOT NULL,
+            year            INTEGER NOT NULL,
+            cover           TEXT,
+            duration        INTEGER NOT NULL,
+            sample_rate     INTEGER,
+            bits_per_sample INTEGER,
+            created_at      INTEGER NOT NULL DEFAULT (unixepoch())
+        )
+        "#,
+    )
+    .execute(&mut *transaction_pool)
+    .await
+    .map_err(|e| format!("Soitin DB: Failed to create albums table: {}", e))?;
+
+    // Tracks table
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS tracks (
+            id          TEXT PRIMARY KEY,
+            album_id    TEXT NOT NULL REFERENCES albums(id) ON DELETE CASCADE,
+            title       TEXT NOT NULL,
+            artist      TEXT NOT NULL,
+            track_index INTEGER,
+            duration    INTEGER NOT NULL,
+            file_path   TEXT NOT NULL UNIQUE,
+            created_at  INTEGER NOT NULL DEFAULT (unixepoch())
+        )
+        "#,
+    )
+    .execute(&mut *transaction_pool)
+    .await
+    .map_err(|e| format!("Soitin DB: Failed to create tracks table: {}", e))?;
+
+    transaction_pool.commit().await.map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+// pub async fn get_album_with_tracks
