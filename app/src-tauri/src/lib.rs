730c0f24b6050974eeb77716f305fcd4e9ade60a
lauriahlfors <lauriahlfors@proton.me>
2026-09-13 11:47:12 +0300

    feat(backend): add database initialization to run on app startup

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,8 +1,24 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
+// Imports
+mod music_db;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::block_on(async move {
+                let pool = music_db::initialize_database(&handle)
+                    .await
+                    .expect("[soitin]: Failed to initialize database");
+                handle.manage(pool);
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
